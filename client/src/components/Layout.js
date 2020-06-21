9baa13fdb8797456fecf23cfbf37a395df1e824a
FlorianSahbi <florian.sahbi@gmail.com>
2020-06-21 16:32:54 +0200

    Login screen style

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,12 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-function Layout({ children }) {
+function Layout({ children, header = true, footer = true }) {
   return (
     <>
-      <Header />
+      {header && <Header />}
       {children}
-      <Footer />
+      {footer && <Footer />}
     </>
   )
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import { LOGIN } from "../graphql/authentication";
 import FacebookLogin from "react-facebook-login";
+import Layout from "../components/Layout";
+import logo from "../logo.svg";
 
 let users = [
   {
@@ -22,6 +24,25 @@ let users = [
     email: "florian.sahbi@gmail.com",
   },
 ]
+
+let inputsStyle = {
+  text: {
+    height: "40px",
+    width: "100%",
+    padding: "1rem",
+    border: "1px solid grey",
+    outline: "unset",
+  },
+  submit: {
+    height: "40px",
+    width: "100%",
+    padding: "1rem",
+    border: "1px solid grey",
+    outline: "unset",
+    background: "#fff"
+  }
+}
+
 
 function Login() {
   console.log(("Login"))
@@ -65,20 +86,28 @@ function Login() {
   const onSubmit = data => login({ variables: { identifier: data.email, password: data.password } });
 
   return (
-    <>
-      <FacebookLogin
-        appId="267253924584068"
-        autoLoad={false}
-        fields="name,email,picture.height(1000).width(1000)"
-        onClick={() => console.log("ok")}
-        callback={responseFacebook}
-      />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="email" ref={register({ required: true })} />
-        <input name="password" ref={register({ required: true })} />
-        <input type="submit" />
-      </form>
-    </>
+    <Layout header={false} footer={false}>
+      <div style={{ display: "grid", backgroundColor: "#121212", gridTemplateColumns: "1fr 1fr", height: "100vh", width: "100%" }}>
+        <div style={{ height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <img src={logo} />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+          <form style={{ display: "grid", gridTemplateRows: "1fr 1fr 1fr", gap: ".5rem" }} onSubmit={handleSubmit(onSubmit)}>
+            <input style={inputsStyle.text} name="email" ref={register({ required: true })} />
+            <input style={inputsStyle.text} name="password" ref={register({ required: true })} />
+            <input style={inputsStyle.submit} type="submit" />
+          </form>
+          <FacebookLogin
+            appId="267253924584068"
+            autoLoad={false}
+            fields="name,email,picture.height(1000).width(1000)"
+            onClick={() => console.log("ok")}
+            callback={responseFacebook}
+            style={{ border: "2px solid pink" }}
+          />
+        </div>
+      </div>
+    </Layout>
   )
 }
 
